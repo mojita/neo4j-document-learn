ca0c837e20f78cabe720978328ace836f425df64
mojita <18482107760@163.com>
2019-03-06 10:41:48 +0800

    提交内容

---
+++ document-learn/src/main/java/com/mojita/neo4j/documentlearn/DocumentLearnApplication.java
@@ -0,0 +1,15 @@
+package com.mojita.neo4j.documentlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentLearnApplication {
+
+    public static void main(String[] args) {
+        //SpringApplication.run(DocumentLearnApplication.class, args);
+
+
+    }
+
+}
---
+++ document-learn/src/test/java/com/mojita/neo4j/documentlearn/DocumentLearnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mojita.neo4j.documentlearn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DocumentLearnApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
